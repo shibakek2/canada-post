2b2a24c95a28fc40a873cf819667201e9ff8aaf5
ShibaKEK <wilhemnorman732@GMAIL.COM>
2024-07-17 03:15:28 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,10 @@
 from canadapostwrapper.canadapost import CanadaPostTracker
-tracker = CanadaPostTracker(tracking_id='YOUR_TRACKING_ID')
-#tracker.start_tracking()  this is broken and is being removed. you can put the tracker in a loop and continue to call it your self. 
 
+tracker = CanadaPostTracker(tracking_id='6107031730947239')
 
+shipping_info = tracker.check_shipping()
 
+if shipping_info:
+    print(shipping_info)
+else:
+    print("Failed to retrieve shipping information.")
