d6eebd25b40d82a16c0a091e39176f29e028604a
ShibaKEK <wilhemnorman732@GMAIL.COM>
2024-07-17 03:07:52 -0400

    Update example.py
    updated code

--- example.py
+++ example.py
@@ -1,3 +1,6 @@
 from canadapostwrapper.canadapost import CanadaPostTracker
 tracker = CanadaPostTracker(tracking_id='YOUR_TRACKING_ID')
-tracker.start_tracking()
+#tracker.start_tracking()  this is broken and is being removed. you can put the tracker in a loop and continue to call it your self. 
+
+
+
