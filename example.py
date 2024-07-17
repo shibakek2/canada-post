29b90aaf6ee1206a50fc11f48c20d6c5dae70944
ShibaKEK <wilhemnorman732@GMAIL.COM>
2024-07-17 02:48:37 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,14 +1,3 @@
-from canadapostwrapper import CanadaPostTracker
-
-def main():
-    tracking_id = '6107031730947239'
-    tracker = CanadaPostTracker(tracking_id=tracking_id)
-    shipping_info = tracker.check_shipping()
-    if shipping_info:
-        print("Initial Shipping Info:")
-        print(shipping_info)
-    print("Started continuous tracking.")
-    tracker.start_tracking()
-
-if __name__ == "__main__":
-    main()
+from canadapostwrapper.canadapost import CanadaPostTracker
+tracker = CanadaPostTracker(tracking_id='YOUR_TRACKING_ID')
+tracker.start_tracking()
